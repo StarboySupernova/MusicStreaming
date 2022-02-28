ea50ba7313647e4fcfe029af9ba0c11c55acf719
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-02-28 22:30:22 +0000

    Initial Commit

---
+++ MusicStreaming/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicStreaming
+//
+//  Created by user213454 on 2/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicStreaming/MusicStreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicStreamingApp.swift
+//  MusicStreaming
+//
+//  Created by user213454 on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicStreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
